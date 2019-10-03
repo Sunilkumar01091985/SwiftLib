61b4bd8abdbfcf068088e0d6fecbc6dc729b21c9
Sunilkumar Bassappa <sunilkumar@ISSLT465.local>
2019-10-03 18:36:56 +0530

    Initial Project setup

---
+++ SwiftLib/SwiftLib.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLib.h
+//  SwiftLib
+//
+//  Created by Sunilkumar Bassappa on 01/10/19.
+//  Copyright © 2019 iNube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftLib.
+FOUNDATION_EXPORT double SwiftLibVersionNumber;
+
+//! Project version string for SwiftLib.
+FOUNDATION_EXPORT const unsigned char SwiftLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLib/PublicHeader.h>
+
+
